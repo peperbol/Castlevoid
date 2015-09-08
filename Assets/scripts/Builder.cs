54088f170fdbc1f9b47562cd68d7603880b35a7f
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-09-08 16:01:46 +0200

    updated colors

--- Assets/scripts/Builder.cs
+++ Assets/scripts/Builder.cs
@@ -10,34 +10,59 @@ public class Builder : RadialMovementInput {
     public House housePrefab;
     public Base team;
     public bool buildmode;
+    public LayerMask BuildObstruction;
+    public float buildSpace;
+    public bool BuildMode{
+        get{ return buildmode; }
+        set{
+            buildmode = value;
+
+            buildVisual.enabled = value;
+        }
+    }
+
+
+    public Renderer buildVisual;
+    public Material buildPositive;
+    public Material buildNegative;
+
+    protected bool CanBuild() {
+        return Physics2D.OverlapCircle(transform.position, buildSpace, BuildObstruction) == null;
+    }
     protected override void Update()
     {
         base.Update();
         if (inUse) {
             if (Input.GetAxis(Build) > deadZone) {
-                buildmode = true;
+                BuildMode = true;
             }
-            if (buildmode) {
-
-                if (Input.GetButtonDown(Special1)) {
-                    House.Build(housePrefab, team, Position);
-                    buildmode = false;
-                }
-                else if (Input.GetButtonDown(Special2))
+            if (BuildMode) {
+                if (CanBuild())
                 {
+                    if (Input.GetButtonDown(Special1))
+                    {
+                        House.Build(housePrefab, team, Position);
+                        BuildMode = false;
+                    }
+                    else if (Input.GetButtonDown(Special2))
+                    {
 
-                    buildmode = false;
-                }
-                else if (Input.GetButtonDown(Special3))
-                {
+                        BuildMode = false;
+                    }
+                    else if (Input.GetButtonDown(Special3))
+                    {
 
-                    buildmode = false;
+                        BuildMode = false;
+                    }
                 }
             }
 
             if (Input.GetButtonDown(attack)) {
 
             }
+        }
+        if (buildmode) {
+            buildVisual.material = (CanBuild()) ? buildPositive : buildNegative;
         }
     }
 }
