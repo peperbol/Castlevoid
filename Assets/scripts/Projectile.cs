f9c746bf0f5289a401731418dbeee6a530615557
Pepijn Willekens <pepijnwillekens@gmail.com>
2016-04-09 17:19:30 +0200

    new projectile visuals, and tweaked explosions

--- Assets/scripts/Projectile.cs
+++ Assets/scripts/Projectile.cs
@@ -15,11 +15,13 @@ public class Projectile : MonoBehaviour
     public float steerForce = 1;
     public Transform target;
     public AudioClip hitSound;
+    public Material hitMaterial;
     Rigidbody2D rb;
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        lockRange = ((Vector2)(target.position - transform.position)).magnitude / 2;
     }
     bool locked;
     float lockRange;
@@ -30,7 +32,7 @@ public class Projectile : MonoBehaviour
         timeToLive -= Time.fixedDeltaTime;
         if (timeToLive <= 0 || target == null || target.GetComponent<Collider2D>() == null)
         {
-            Destroy(gameObject);
+            StartCoroutine(waitToDestroy());
             return;
         }
         Vector2 dir = ((Vector2)(target.position - transform.position));
@@ -49,10 +51,7 @@ public class Projectile : MonoBehaviour
         {
             rb.velocity = dir.normalized * steerForce;
         }
-    }
-    void start()
-    {
-        lockRange = ((Vector2)(target.position - transform.position)).magnitude /2;
+
     }
     void OnCollisionEnter2D(Collision2D coll)
     {
@@ -60,8 +59,30 @@ public class Projectile : MonoBehaviour
         if (a != null)
         {
             a.Damage(this);
-            Destroy(gameObject);
+            StartCoroutine(waitToDestroy());
             AudioPlay.PlaySound(hitSound, 0.05f);
         }
+    }
+    IEnumerator waitToDestroy()
+    {
+        Destroy(GetComponent<Collider2D>());
+        Destroy(GetComponent<SpriteRenderer>());
+        var tr = GetComponent<TrailRenderer>();
+        float t = tr.time;
+        Color c = tr.material.color;
+        Color e = tr.material.GetColor("_EmissionColor");
+        while (t> 0)
+        {
+            c = Color.Lerp(c, Color.white, 0.2f);
+            e = Color.Lerp(e, Color.white, 0.2f);
+
+            tr.material.color = c;
+            tr.material.SetColor("_EmissionColor", e);
+            //tr.material.color = 
+            t -= Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
